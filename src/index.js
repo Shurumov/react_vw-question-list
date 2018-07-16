e7e4cbf5646f86fa4a35651623630afafb065940
Aleksei <alexei@0113@gmail.com>
2018-07-16 19:23:10 +0700

    changes

--- src/index.js
+++ src/index.js
@@ -2,8 +2,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import form from './form.json'
+import logo from './vw.png';
 
+const Welcome = (props) => {
+	const {beginTest} = props;
+	return  <div className="welcome-page">
+						<img src={logo} alt=""/>
+						<h2>Ответьте на 10 вопросов о Volkswagen Touareg</h2>
+						<BeginTest beginTest={beginTest} />
+					</div>
+}
+
+const BeginTest = (props) => {
+	const {beginTest} = props;
+	return <div	className="btn btn-begin" onClick={() => beginTest()}>
+						Начать тест
+					</div>
+}
 
 const Questions = (props) => {
 	const { questions, saveResponse, toggleResponseCheckbox } = props;
@@ -87,15 +102,23 @@ const CheckBoxList = (props) => {
 }
 
 const SendButton = (props) => {
-	const { sendData } = props;
+	const { sendData, userId, formId, baseUrl, projectName, session } = props;
 
 	return(
 		<div	className="btn btn-send" 
-					onClick={() => sendData(1, 2, "https://api.appercode.com/v1/", "volkswagen", "3f5e1949-3b66-473c-bda3-a317084ae753")}>
+					onClick={() => sendData(userId, formId, baseUrl, projectName, session)}>
 					Отправить
 		</div>
 	)
 }
+
+const ScreenGratitude = () => {
+	return <div className="gratitude-page">
+						<img src={logo} alt=""/>
+						<h2>Ваши ответы сохранены, спасибо</h2>
+				</div>
+}
+
 
 class App extends React.Component {
 
@@ -103,49 +126,133 @@ class App extends React.Component {
 		super(props);
 		this.state = {
 			idAndAnswers: null,
-			questions: []
+			questions: [],
+			displayBlocks:
+				{
+					welcome: false,
+					questions: false,
+					gratitude: false
+				}
 		};
 		this.saveResponseRadio = this.saveResponseRadio.bind(this);
 		this.toggleResponseCheckbox = this.toggleResponseCheckbox.bind(this);
 		this.sendData = this.sendData.bind(this);
+		this.beginTest = this.beginTest.bind(this);
+	}
+
+	beginTest(){
+		const {displayBlocks} = this.state;
+		displayBlocks.welcome = false;
+		displayBlocks.questions = true;
+		this.setState({
+			displayBlocks : displayBlocks
+		})
 	}
 	
+	
 	componentDidMount() {
-		const partsFromJson = form.parts;
+
+		function getForm (userId, formId, baseUrl, projectName, session, app) {
+			var xhr = new XMLHttpRequest();
+			xhr.open('GET', baseUrl + projectName + "/forms/" + formId);
+			xhr.setRequestHeader('X-Appercode-Session-Token', session);
+			xhr.send();
+	
+			xhr.onreadystatechange = function () {
+				if (xhr.readyState !== 4)
+					return;
+				if (xhr.status === 401) {
+					//loginByToken();
+				} else
+				if (xhr.status !== 200) {
+					alert(xhr.status + ': ' + xhr.statusText);
+				} else {
+					var response = JSON.parse(xhr.responseText);
+					partsFromJson = response.parts;
+					
+					checkExecution(userId, formId, app)
+				}
+			}
+		}
+
+		function checkExecution(userId, formId){
+			var xhr = new XMLHttpRequest();
+			xhr.open('GET', baseUrl + projectName + "/objects/Answers?include=%5B'userId'%2C'formId'%5D&take=-1");
+			xhr.setRequestHeader('X-Appercode-Session-Token', session);
+			xhr.send();
+	
+			xhr.onreadystatechange = function () {
+				if (xhr.readyState !== 4)
+					return;
+				if (xhr.status === 401) {
+					//loginByToken();
+				} else
+				if (xhr.status !== 200) {
+					alert(xhr.status + ': ' + xhr.statusText);
+				} else {
+					var response = JSON.parse(xhr.responseText);
+					
+					if(response.some(item => (
+						(item.userId == userId && item.formId == formId)
+					))){
+						const displayBlocks = app.state.displayBlocks
+						displayBlocks.gratitude = true;
+						app.setState({
+							displayBlocks: displayBlocks
+						})
+					} else {
+						createQuestionsForUser(app);
+					}
+					
+				}
+			}
+		}
+
+		function createQuestionsForUser(app){
+
+			while (countQuestions < 10){
+				const randomValue = Math.floor(Math.random()*partsFromJson.length);
+				if (selectedIdQuestions.indexOf(randomValue) === -1)
+				{
+					selectedIdQuestions.push(randomValue);
+					countQuestions++;
+				}
+			}
+	
+			partsFromJson.forEach(item => {
+				const idControl = item.sections[0].groups[0].controls[0].id;
+				const typeControl = item.sections[0].groups[0].controls[0].type
+				if (typeControl === "checkBoxList"){ 
+					idAndAnswers[idControl] = [];
+				} else {
+					idAndAnswers[idControl] = null
+				}
+			});
+	
+			selectedIdQuestions.sort();
+			
+			selectedIdQuestions.map(item => (
+				questionsForUser.push(partsFromJson[item].sections[0].groups[0].controls[0])
+			));
+			
+			const displayBlocks = app.state.displayBlocks
+			displayBlocks.welcome = true
+			app.setState({
+				questions: questionsForUser,
+				idAndAnswers: idAndAnswers,
+				displayBlocks: displayBlocks
+			})
+		}
+
+		const {userId, formId, baseUrl, projectName, session} = this.props;
+		let partsFromJson;
 		let questionsForUser =[];
 		let countQuestions = 0;
 		let idAndAnswers = {}
 		const selectedIdQuestions=[];
-	
-		while (countQuestions < 10){
-			const randomValue = Math.floor(Math.random()*partsFromJson.length);
-			if (selectedIdQuestions.indexOf(randomValue) === -1)
-			{
-				selectedIdQuestions.push(randomValue);
-				countQuestions++;
-			}
-		}
+		const app = this;
+		getForm(userId, formId, baseUrl, projectName, session, app);
 
-		partsFromJson.forEach(item => {
-			const idControl = item.sections[0].groups[0].controls[0].id;
-			const typeControl = item.sections[0].groups[0].controls[0].type
-			if (typeControl === "checkBoxList"){ 
-				idAndAnswers[idControl] = [];
-			} else {
-				idAndAnswers[idControl] = null
-			}
-		});
-
-		selectedIdQuestions.sort();
-		
-		selectedIdQuestions.map(item => (
-			questionsForUser.push(partsFromJson[item].sections[0].groups[0].controls[0])
-		));
-		
-		this.setState({
-			questions: questionsForUser,
-			idAndAnswers: idAndAnswers
-		})
 	}
 
 	saveResponseRadio(idQuestion, valueAnswer){
@@ -177,21 +284,15 @@ class App extends React.Component {
 
 	sendData(userId, formId, baseUrl, project, session) {
 
-
 		const idAndAnswers = this.state.idAndAnswers;
-
 		const result = JSON.stringify(idAndAnswers);
-
-
 		const body = '{"userId":"'+ userId+
 				'","formId":"'+ formId+
 				'","result":'+ result+
 				',"status": "new"}';
 
-		console.log(body);
 		const xhr = new XMLHttpRequest();
 		const url = baseUrl + project + "/objects/Answers";
-		console.log(url);
 		xhr.open("POST", url, true);
 		xhr.setRequestHeader("Content-Type", "application/json");
 		xhr.setRequestHeader('X-Appercode-Session-Token', session);
@@ -204,40 +305,61 @@ class App extends React.Component {
 		};
 		xhr.send(body);
 		
+		const {displayBlocks} = this.state;
+		displayBlocks.questions = false;
+		displayBlocks.gratitude = true;
+		this.setState({
+			displayBlocks: displayBlocks
+		})
 	}
 
   render() {
-		const { questions } = this.state;
-		const { saveResponseRadio, toggleResponseCheckbox, sendData } = this;
+		const { questions, displayBlocks } = this.state;
+		const { saveResponseRadio, toggleResponseCheckbox, sendData, beginTest } = this;
+		const { userId, baseUrl, projectName, session, formId} = this.props;
     return (<div className="app_wrapper">
+							{displayBlocks.welcome ? <Welcome beginTest ={beginTest}/> : null}
+
+							{displayBlocks.questions ? 
 							<Questions	questions = { questions} 
 													saveResponse ={ saveResponseRadio }
 													toggleResponseCheckbox = { toggleResponseCheckbox } />
-							<SendButton sendData = { sendData } />
+							: null}
+
+							{displayBlocks.questions ? 
+							<SendButton sendData = { sendData }
+													session={session} 
+													userId={userId}
+													baseUrl={baseUrl}
+													projectName={projectName} 
+													formId = {formId}
+							/> : null}
+							
+							{displayBlocks.gratitude ?
+							<ScreenGratitude/> :
+							null
+							}
 						</div>
     );
   }
 }
 
 function sessionFromNative(e) {
-	alert(e);
   const userData = JSON.parse(e);
   const session = userData.sessionId;
   const userId = userData.userId;
-  const project = userData.projectName;
+  const projectName = userData.projectName;
   const baseUrl = userData.baseUrl;
-	const refreshT = userData.refreshToken;
 
 	
-  /*ReactDOM.render(<App 	session = {session}
-												userId = {userId}
-												project = {project}
-												baseUrl = {baseUrl}
-												refreshT = {refreshT}/>, 
-									document.getElementById('root'));*/
+	ReactDOM.render(<App 
+										session={session} 
+										userId={userId}
+										baseUrl={baseUrl}
+										projectName={projectName}
+										formId={2}
+									/>, document.getElementById('root'));
 }
-ReactDOM.render(<App />, document.getElementById('root'));
-sessionFromNative('{"sessionId":"3f5e1949-3b66-473c-bda3-a317084ae753","userId":"1","project": "volkswagen","baseUrl":"https://api.appercode.com/v1/","refreshT":"1"}')
-/*
-'{"sessionId":"29ba0dce-a7c0-442a-84f1-49daea0647e0","userId":"1","project": "volkswagen","baseUrl":"https://api.appercode.com/v1/","refreshT":"1"}'
-*/
+
+sessionFromNative('{"sessionId":"6794fef7-4b92-4c7a-91a2-3417cf383097","userId":"1","projectName": "volkswagen","baseUrl":"https://api.appercode.com/v1/","refreshT":"1"}')
+
